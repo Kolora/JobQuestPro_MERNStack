1f85e6cfea39e955ffb99bf77d7921b878972f0a
Divya <divya.kolora@gmail.com>
2023-09-01 10:30:24 -0600

    added model

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -4,15 +4,19 @@ const jwt = require("jsonwebtoken");
 function authorize(req, res, next) {
   console.log("authorizing...");
   try {
-    //check request has token in authorization header
+    //check request has token sent in authorization header
     let token = req.header("Authorization");
     if (!token) {
       return res.status(403).json({ error: "No token available" });
     }
     console.log("token :", token);
 
+    // token comes in the format "Bearer <token>", so must edit the string
+
     token = token.replace("Bearer", "");
     console.log(token);
+
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
 
     //check token is not expired and is valid
     if (payload.error) {
