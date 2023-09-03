14a48023efced48676094339310162b276f6118f
Divya <divya.kolora@gmail.com>
2023-09-03 11:24:34 -0600

    database connected and controller set up

---
+++ backend/dbConfig/dbConnection.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const dbConnection = async () => {
+  try {
+    const dbConnection = await mongoose.connect(process.env.MONGO_URL);
+
+    console.log("Database Connected Successfully");
+  } catch (error) {
+    console.log("Database Error: " + error);
+  }
+};
+
+export default dbConnection;
--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,38 @@
+import bodyParser from "body-parser";
+import xss from "xss-clean";
+import morgan from "morgan";
+import mongoSanitize from "express-mongo-sanitize";
+
 // To run server import express, use app variable to use
-const express = require("express");
+// const express = require("express");
+import express from "express";
 const app = express();
 
 //enable env variables
-require("dotenv").config();
+// require("dotenv").config();
+import dotenv from "dotenv";
+dotenv.config();
 
 //import cors to connect server and client
-const cors = require("cors");
+// const cors = require("cors");
+import cors from "cors";
 
 //json middleware to connect server and client
 app.use(express.json());
 app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(morgan("dev"));
 
 //connect to database via config file
-const connectDatabase = require("./config");
-connectDatabase();
+import dbConnection from "./dbConfig/dbConnection.js";
+// const connectDatabase = require("./config");
+dbConnection();
 
 //set port
 const PORT = 8080;
